cd60f969744e9cb9107d7a6c187ccf7c1aa8efa6
thenav56 <ayernavin@gmail.com>
2024-08-09 13:05:24 +0545

    Add option to provide custom env file for psql-connect helper

--- psql-connect.sh
+++ psql-connect.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-export $(cat .env | grep '^CONNECT_' | xargs)
+if [ -z "$1" ]; then
+    echo "No env path supplied"
+    exit 1
+else
+    echo "Using env file: $1"
+    set -o allexport
+    source "$1" set
+    set +o allexport
+fi
 
 psql "sslmode=verify-full password=$CONNECT_PASSWORD user=$CONNECT_USER host=$CONNECT_HOST port=$CONNECT_PORT sslcert=$CONNECT_SSLCERT sslkey=$CONNECT_SSLKEY sslrootcert=$CONNECT_SSLROOTCERT dbname=$CONNECT_DBNAME"
 
